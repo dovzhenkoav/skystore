1e6b107abf0b3dc1c830eeea8b91b3269a45b73a
OFAxTAB <rockenny@ymail.com>
2023-07-12 11:02:19 +0300

    init

---
+++ app_catalog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index_view(request):
+    return render(request, 'catalog/index.html')
+
+
+def contacts_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} {phone} {message}')
+    return render(request, 'catalog/contacts.html')
+
+
+
+
+
+
+
+
