ae6632535777f0bc406bcfaef33d9ec304fab148
OFAxTAB <rockenny@ymail.com>
2023-07-12 12:39:54 +0300

    HW_20_1

--- app_catalog/models.py
+++ app_catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', verbose_name='Превью')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+    created_at = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
+    last_changed_at = models.DateTimeField(verbose_name='дата последнего изменения', auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
